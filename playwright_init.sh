b1a09b2a094e9047f309143244e46fa6acee5322
Taylor Monacelli <taylormonacelli@gmail.com>
2024-11-16 19:25:03 -0800

    ci

--- playwright_init.sh
+++ playwright_init.sh
@@ -9,7 +9,6 @@ corepack prepare pnpm@latest --activate
 pnpm --version
 
 pnpm create playwright --browser=chromium --install-deps --quiet --gha --lang=Typescript
-echo node_modules >>.gitignore
 
 pnpm add --save-dev --save-exact typescript
 pnpm add --save-dev --save-exact prettier
